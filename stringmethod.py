d6cb9a9f86a70a572b05e8e9ee89cd3929fc2f46
keshav-bansal18 <46074720+keshav-bansal18@users.noreply.github.com>
2019-04-04 10:36:55 +0530

    first question solution of 2nd mid solution

---
+++ stringmethod.py
@@ -0,0 +1,9 @@
+s='keshav bansal'
+print('title=====',s.title())
+
+print('upper===',s.upper())
+
+print('lower====',s.lower())
+
+print('count a===',s.count('a'))
+print('swapcase===',s.swapcase())
